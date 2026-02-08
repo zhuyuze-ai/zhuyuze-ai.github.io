311efc606004ba88d9c3ea07b0bc08c072e10a4b
sunrise-ikun <zhuyuze23@mails.ucas.ac.cn>
2026-02-08 17:32:38 +0000

    Deploying to gh-pages from @ zhuyuze-ai/zhuyuze-ai.github.io@380076de380d2469173cd3b43abb991f6f7eebc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -377,16 +377,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-setting-up-my-personal-site-as-a-new-beginning-looking-forward-to-the-future-with-great-anticipation",
+          title: 'Setting up my personal site as a new beginning. Looking forward to the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
